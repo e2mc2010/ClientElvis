a7f4fdeaabfb8f8bab3997e3e1d2fc94b75421a3
Elvis Masache <e2mc2010@gmail.com>
2024-05-10 16:05:47 -0400

    practice

--- 1_frontend/ejercicio_7/script.js
+++ 1_frontend/ejercicio_7/script.js
@@ -1,24 +1,14 @@
 function js() {
     let num = document.getElementById("number").value;
-
-    LOOP
-
-let i = 0; cambiar el valor
-const u = ; no cambia
-
-mi_varibale =  input 
-
- for ( let i = 0 ; i < variable ; i++ ){
-    // codig
-    // codig
-    // codig
-    // codig
-    // codig
-  i = 10
 }
-// cdnoscncdscd
-// scjkndckdc
-// ncdkn
+ for ( let i = 0 ; i < 80 ; i++ ){
+    if (i % 2 != 0) {
+    
+    }
+}
+i = 10
+}
+
     
     if (num %2 === 0) {
         document.getElementById("result").innerHTML = num + "is Even";
