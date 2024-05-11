427743ce2778bb871a93b5b6a8cc6d84b49a312c
Elvis Masache <e2mc2010@gmail.com>
2024-05-11 00:44:53 -0400

    Practice

--- 1_frontend/ejercicio_8/script.js
+++ 1_frontend/ejercicio_8/script.js
@@ -1,27 +1,30 @@
 
-let limitNumbers = document.getElementById("limit").value;
-let numbers = [];
-
 function even() {
+
+    let limitNumbers = document.getElementById("limit").value;
+    let numbers = [];
+
+
     for (let i = 0; i <= limitNumbers; i++) {
         if (i % 2 == 0)
             numbers.push(i);
-      
+
     }
-    console.log(numbers);
- 
-document.getElementById("results").innerHTML = numbers.join(", ");        
+
+    document.getElementById("results").innerHTML = numbers.join(", ");
 }
 
 function odd() {
+    let limitNumbers = document.getElementById("limit").value;
+    let numbers = [];
+
     for (let i = 1; i <= limitNumbers; i++) {
         if (i % 2 == 1)
 
             numbers.push(i);
     }
-    console.log(numbers);
 
-    document.getElementById("results").innerHTML = numbers.join(", ");   
+    document.getElementById("results").innerHTML = numbers.join(", ");
 }
- 
+
 
