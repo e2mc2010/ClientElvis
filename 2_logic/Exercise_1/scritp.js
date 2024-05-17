925319f6276292ab3892de8d221c78f5742e9b63
ErikaMasacheM <16kmasache@gmail.com>
2024-05-16 22:56:46 -0500

    first logic

---
+++ 2_logic/Exercise_1/scritp.js
@@ -0,0 +1,21 @@
+//Cuantas manzanas tengo en mi arreglo
+// Algoritmo
+// 1 my variable to numbers of apples (my_counter)
+// 2 iterate over an array
+// 3 ask if it is an apple
+// 4 increace my counter
+// 5 end the Iterator
+// 6 return my counter
+
+const fruitsNames = ['apple', 'banana','pear','apple', 'apple'] ;
+
+let my_counter = 0;
+for (let i =0; i < fruitsNames.length; i++) {
+    if(fruitsNames[i] == 'apple'){
+        my_counter++;
+    }
+}
+
+
+console.log('I have '+my_counter+' apples');
+
